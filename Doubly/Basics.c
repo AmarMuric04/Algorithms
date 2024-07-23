96a7310e4d7f636df9bad3096d81449127dbe5e7
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-07-23 22:56:36 +0200

    More DLL

--- Doubly/Basics.c
+++ Doubly/Basics.c
@@ -96,6 +96,71 @@ void add_at(struct node **head, struct node **tail, int position, int data)
   }
 }
 
+void delete_first(struct node **head)
+{
+  if ((*head)->next == NULL)
+  {
+    printf("List removed.");
+    free(*head);
+    *head = NULL;
+  }
+  *head = (*head)->next;
+  free((*head)->prev);
+  (*head)->prev = NULL;
+}
+
+void delete_last(struct node **tail)
+{
+  if (*tail == NULL)
+  {
+    printf("List doesn't exist");
+    return;
+  }
+
+  *tail = (*tail)->prev;
+  free((*tail)->next);
+  (*tail)->next = NULL;
+}
+
+void delete_at(struct node **head, struct node **tail, int position)
+{
+  int length = get_list_length(*head);
+  if (position < 0)
+  {
+    printf("Can't insert a node in a negative position.\n");
+    return;
+  }
+  else if (position >= length)
+  {
+    printf("Can't insert a node in the given position.\n");
+    return;
+  }
+  else if (position == 0)
+  {
+    delete_first(head);
+    return;
+  }
+  else if (position == length - 1)
+  {
+    delete_last(tail);
+    return;
+  }
+  else
+  {
+    struct node *current = *head;
+    for (size_t i = 0; i < position; ++i)
+    {
+      current = current->next;
+    }
+    current->prev->next = current->next;
+    if (current->next != NULL)
+    {
+      current->next->prev = current->prev;
+    }
+    free(current);
+  }
+}
+
 void print_nodes(struct node *head)
 {
   struct node *current = head;
@@ -105,6 +170,37 @@ void print_nodes(struct node *head)
     current = current->next;
   }
   printf("\n");
+}
+
+void clear_list(struct node **head)
+{
+  while ((*head)->next != NULL)
+  {
+    free((*head)->next);
+    (*head)->next = NULL;
+  }
+  free(*head);
+  *head = NULL;
+}
+
+void reverse(struct node **head, struct node **tail)
+{
+  struct node *temp = NULL;
+  struct node *current = *head;
+  while (current != NULL)
+  {
+    temp = current->prev;
+    current->prev = current->next;
+    current->next = temp;
+    current = current->prev;
+  }
+
+  if (temp != NULL)
+    *head = temp->prev;
+
+  struct node *swap_temp = *head;
+  *tail = *head;
+  *tail = swap_temp;
 }
 
 int main()
@@ -120,11 +216,19 @@ int main()
   add_to_start(&head, 55);
   add_to_end(&tail, 25);
   add_to_end(&tail, 40);
+  delete_first(&head);
+  delete_last(&tail);
   add_at(&head, &tail, -1, 100);
 
   print_nodes(head);
+  reverse(&head, &tail);
+  print_nodes(head);
 
   printf("%d", get_list_length(head));
+
+  clear_list(&head);
+  if (head == NULL)
+    printf("\nList deleted.\n");
 
   return 0;
 }
